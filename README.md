4e6ee07be3694d10181d0e9a3622376928dc0074
Wenny Hidayat <wenny.hidayat@digio.com.au>
2021-02-12 16:58:57 +1100

    Sample Consumer method to access state

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Search for keywords e.g. `2. Sample nested Provider` to see where they're implem
 - 2. Sample nested Provider: \<Product>provider, for product details listener. This is a sample of nested provider.
 - 3. Opt-out listener: to subscribe and get the init state, but ignore subsequent updates.
 - 4. Sample Provider with rebuild on value change. Aside from its brevity (no passing on unused context), it also specifies the instruction to rebuild on value change instead of merely recycling the Widget as per the `builder` method.
+- 5. Provider access outside build method: parent-level wrapper, which is the reverse of the child-level ChangeNotifierProvider Provider method so far. Consumer\<Product> => (ctx, product, child) => Widget() is another way of accessing state, which is useful for giving access to state without having to access its build method, such as in the case of primitive Widgets. In `product_item.dart`, the whole Widget now builds only based off the first context snapshot, but subsequent changes will rebuild the heart `IconButton`.
 
 ## Important Notes
 - Nested providers can cause bugs - because Flutter recycles Widgets and doesn't rebuild on every navigation, nested Providers may have stale data. This happens on virtualised lists that don't get rendered when they are off-screen.
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -15,28 +15,34 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 2. Sample nested Provider
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context,
+        listen: false); // only renders once. See 5. Provider access outside build method
     return ClipRRect(
       // Styling widget to wrap a rectangle and reshape it into rounded corners.
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context)
-                .pushNamed(ProductDetailScreen.routeName, arguments: product.id);
-                // .pushNamed(ProductDetailScreen.routeName, arguments: id);
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+            // .pushNamed(ProductDetailScreen.routeName, arguments: id);
           },
           child: Image.network(product.imageUrl, fit: BoxFit.cover),
           // child: Image.network(imageUrl, fit: BoxFit.cover),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: product.isFavourite ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
-            onPressed: () {
-              product.toggleFavourites();
-            },
-            color: Theme.of(context).accentColor,
+          // 5. Provider access outside build method
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+              icon: product.isFavourite
+                  ? Icon(Icons.favorite)
+                  : Icon(Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavourites();
+              },
+              color: Theme.of(context).accentColor,
+            ),
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
